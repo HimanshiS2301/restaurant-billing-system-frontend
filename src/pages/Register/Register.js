a23bde5ef4905c1d55e57d7833a2fb7f77f94958
Himanshi Sherwani <himanshi2301s@gmail.com>
2024-06-04 22:36:03 +0530

    signup page changes

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React,{useState} from "react";
 import Axios from 'axios';
 import "./Register.css";
 
 const Register = () => {
 
-const onRegistration = () => {
+const [formData,setFormData] = useState({
+  name : "",
+  email : "",
+  password : "",
+  confirmpassword : ""
+})
+
+const handleChange = (e) => {
+const {name,value} = e.target;
+setFormData(prevData=>({
+  ...prevData,
+  [name] : value
+}))
+}
+
+const onRegistration = (e) => {
+
+const {name,email,password,confirmpassword} = formData;
+
+if(password!==confirmpassword){
+  alert("Password and Confirm Password do not match !")
+}
+
 Axios.post("http://localhost:5000/api/signup",{
-  username:'akansha',
-  email:'akansha@gmail.com',
-  password:'456789'
+  username:name,
+  email:email,
+  password:password
 })
 .then(res=>console.log('res',res))
-.catch(err=>{})
+.catch(err=>console.log(err))
 }
 
   return (
@@ -22,19 +44,19 @@ Axios.post("http://localhost:5000/api/signup",{
           <h3>Create An Account</h3>
           <br />
           <label for="name">Full Name :</label>
-          <input type="text" className="form-control" name="name" />
+          <input type="text" className="form-control" name="name" value={formData.name} onChange={handleChange} />
         </div>
         <div className="form-group">
           <label for="dob">Email :</label>
-          <input type="text" className="form-control" name="dob" />
+          <input type="text" className="form-control" name="email" value={formData.email} onChange={handleChange} />
         </div>
         <div className="form-group">
           <label for="email">Password :</label>
-          <input type="email" className="form-control" name="email" />
+          <input type="email" className="form-control" name="password" value={formData.password} onChange={handleChange} />
         </div>
         <div className="form-group">
           <label for="address">Confirm Password :</label>
-          <input type="text" className="form-control" name="address" />
+          <input type="text" className="form-control" name="confirmpassword" value={formData.confirmpassword} onChange={handleChange}/>
         </div>
 
         <button type="submit" className="m-3 btn btn-light" onClick={onRegistration}>
